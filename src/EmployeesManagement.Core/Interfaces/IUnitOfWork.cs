85048d27de16065eaac1648ba90401444d40f475
Anton <antongunko@outlook.com>
2021-02-04 17:59:19 +0300

    Add IUnitOfWork

---
+++ src/EmployeesManagement.Core/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using EmployeesManagement.Core.Interfaces.IRepositories;
+using System;
+
+namespace EmployeesManagement.Core.Interfaces
+{
+    public interface IUnitOfWork : IDisposable
+    {
+        ICompanyRepository Company { get; }
+        IEmployeeRepository Employee { get; }
+        ILegalFormRepository LegalForm { get; }
+        IPositionRepository Position { get; }
+        IActivityRepository Activity { get; }
+
+        void Save();
+    }
+}
