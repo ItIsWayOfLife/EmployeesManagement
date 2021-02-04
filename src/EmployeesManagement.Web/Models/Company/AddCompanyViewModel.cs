e9c5fcc3214a2b85f69947d3aa2fa19e00f04928
Anton <antongunko@outlook.com>
2021-02-05 00:06:47 +0300

    Add annotation for edit and add comnany view models

--- src/EmployeesManagement.Web/Models/Company/AddCompanyViewModel.cs
+++ src/EmployeesManagement.Web/Models/Company/AddCompanyViewModel.cs
@@ -1,14 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿
+using System.ComponentModel.DataAnnotations;
 
 namespace EmployeesManagement.Web.Models.Company
 {
     public class AddCompanyViewModel
     {
+        [Required(ErrorMessage = "Укажите название")]
+        [Display(Name = "Название")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина названия")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Укажите название органицазионно-правовой формы")]
+        [Display(Name = "Органицазионно-правовая форма")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина названия органицазионно-правовой формы")]
         public string LegalFormName { get; set; }
+
+        [Required(ErrorMessage = "Укажите название вида деятельности формы")]
+        [Display(Name = "Вид деятельности")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина названия вида деятельности")]
         public string ActivityName { get; set; }
     }
 }
