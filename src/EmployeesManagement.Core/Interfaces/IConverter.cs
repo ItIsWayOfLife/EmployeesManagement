803952e19a4a013e7ef8c928fb7a77455cde3c0d
Anton <antongunko@outlook.com>
2021-02-04 20:17:05 +0300

    Add IConverter

---
+++ src/EmployeesManagement.Core/Interfaces/IConverter.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EmployeesManagement.Core.Interfaces
+{
+    public interface IConverter<TModel, TModelDTO>
+       where TModel : class
+       where TModelDTO : class
+    {
+        TModel ConvertDTOByModel(TModelDTO modelDTO);
+        TModelDTO ConvertModelByDTO(TModel model);
+    }
+}
