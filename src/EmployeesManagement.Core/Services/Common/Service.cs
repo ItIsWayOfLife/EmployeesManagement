fc26ef318a0301744ba56c0d4598c3afeeab0157
Anton <antongunko@outlook.com>
2021-02-04 18:52:50 +0300

    Add Services Common

---
+++ src/EmployeesManagement.Core/Services/Common/Service.cs
@@ -0,0 +1,20 @@
+﻿using EmployeesManagement.Core.Interfaces;
+using System;
+
+namespace EmployeesManagement.Core.Services.Common
+{
+    public abstract class Service : IDisposable
+    {
+        protected IUnitOfWork Database { get; set; }
+
+        public Service(IUnitOfWork uow)
+        {
+            Database = uow;
+        }
+
+        public void Dispose()
+        {
+            Database.Dispose();
+        }
+    }
+}
