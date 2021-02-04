e7c98e743e9c4db583bfc7431dc17ec590063249
Anton <antongunko@outlook.com>
2021-02-04 20:11:07 +0300

    Add CompanyService, upd CompanyDTO

--- src/EmployeesManagement.Core/DTOs/CompanyDTO.cs
+++ src/EmployeesManagement.Core/DTOs/CompanyDTO.cs
@@ -1,10 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
+﻿
 namespace EmployeesManagement.Core.DTOs
 {
     public class CompanyDTO
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Size { get; set; }
+        public string LegalFormName { get; set; }
+        public string ActivityName { get; set; }
     }
 }
---
+++ src/EmployeesManagement.Core/Services/CompanyService.cs
@@ -0,0 +1,45 @@
+﻿using EmployeesManagement.Core.DTOs;
+using EmployeesManagement.Core.Interfaces;
+using EmployeesManagement.Core.Interfaces.IServices;
+using EmployeesManagement.Core.Services.Common;
+using System;
+using System.Collections.Generic;
+
+namespace EmployeesManagement.Core.Services
+{
+    public class CompanyService : Service, ICompanyService
+    {
+        public CompanyService(IUnitOfWork uow) : base(uow)
+        {
+
+        }
+
+        public CompanyDTO Get(int id)
+        {
+            var company = Database.Company.Get(id);
+
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<CompanyDTO> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(CompanyDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(CompanyDTO model)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
