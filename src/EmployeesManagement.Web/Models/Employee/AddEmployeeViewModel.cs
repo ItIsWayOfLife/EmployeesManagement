4d9c826341ba09891e1c85008e5b93d920291a41
Anton <antongunko@outlook.com>
2021-02-05 00:09:39 +0300

    Add annotation for edit and add employee view models

--- src/EmployeesManagement.Web/Models/Employee/AddEmployeeViewModel.cs
+++ src/EmployeesManagement.Web/Models/Employee/AddEmployeeViewModel.cs
@@ -1,14 +1,36 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmployeesManagement.Web.Models.Employee
 {
     public class AddEmployeeViewModel
     {
+        [Required(ErrorMessage = "Укажите имя")]
+        [Display(Name = "Имя")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина имени")]
         public string Firstname { get; set; }
+
+        [Required(ErrorMessage = "Укажите фамилию")]
+        [Display(Name = "Фамилию")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина фамилии")]
         public string Secondname { get; set; }
+
+        [Display(Name = "Отчество")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина отчества")]
         public string Middlename { get; set; }
+
+        [Required(ErrorMessage = "Укажите дату принятия на работу")]
+        [Display(Name = "Дата принятия на работу")]
         public DateTime DateEmployment { get; set; }
+
+        [Required(ErrorMessage = "Укажите должность")]
+        [Display(Name = "Должность")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина должности")]
         public string PositionName { get; set; }
+
+        [Required(ErrorMessage = "Укажите компанию")]
+        [Display(Name = "Компания")]
+        [StringLength(50, ErrorMessage = "Недопустимая длина компании")]
         public string CompanyName { get; set; }
     }
 }
