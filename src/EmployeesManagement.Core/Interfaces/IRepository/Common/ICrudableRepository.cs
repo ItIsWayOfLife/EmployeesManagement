6e97f9ec7b284f156df632038612e8b61e3f10e9
Anton <antongunko@outlook.com>
2021-02-04 16:51:50 +0300

    Add IRepository Common

---
+++ src/EmployeesManagement.Core/Interfaces/IRepository/Common/ICrudableRepository.cs
@@ -0,0 +1,24 @@
+﻿
+namespace EmployeesManagement.Core.Interfaces.IRepository.Common
+{
+    public interface ICrudableRepository<T> : IGetableRepository<T> where T : class
+    {
+        /// <summary>
+        /// Create new model.
+        /// </summary>
+        /// <param name="item">Creatable model.</param>
+        void Create(T item);
+
+        /// <summary>
+        /// Update model.
+        /// </summary>
+        /// <param name="item">Updatable model.</param>
+        void Update(T item);
+
+        /// <summary>
+        /// Delete model.
+        /// </summary>
+        /// <param name="id">Id deletable model.</param>
+        void Delete(int id);
+    }
+}
---
+++ src/EmployeesManagement.Core/Interfaces/IRepository/Common/IGetableRepository.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace EmployeesManagement.Core.Interfaces.IRepository.Common
+{
+    public interface IGetableRepository<T> where T : class
+    {
+        /// <summary>
+        /// Get all models.
+        /// </summary>
+        /// <returns>IEnumerable models.</returns>
+        IEnumerable<T> GetAll();
+
+        /// <summary>
+        /// Get one model.
+        /// </summary>
+        /// <param name="id">Id model.</param>
+        /// <returns>One model.</returns>
+        T Get(int id);
+    }
+}
