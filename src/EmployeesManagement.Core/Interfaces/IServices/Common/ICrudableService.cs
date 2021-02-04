3833ce88d7ffb960dd01e2a1fe87db3510bd9ce5
Anton <antongunko@outlook.com>
2021-02-04 18:42:00 +0300

    Add IServices Common

---
+++ src/EmployeesManagement.Core/Interfaces/IServices/Common/ICrudableService.cs
@@ -0,0 +1,11 @@
+﻿
+namespace EmployeesManagement.Core.Interfaces.IServices.Common
+{
+    public interface ICrudableService<T> : IGetAllService<T> where T : class
+    {
+        T Get(int id);
+        void Add(T model);
+        void Edit(T model);
+        void Delete(int id);
+    }
+}
---
+++ src/EmployeesManagement.Core/Interfaces/IServices/Common/IGetAllService.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace EmployeesManagement.Core.Interfaces.IServices.Common
+{
+    public interface IGetAllService<T> where T : class
+    {
+        public IEnumerable<T> GetAll();
+    }
+}
